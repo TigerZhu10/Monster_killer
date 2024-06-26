5e24d92fc703d0184eb798c52d562c92b519a5fc
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-06-25 20:49:32 -0700

    Init starting page

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ title_text = font.render("MONSTER WRANGLER", True, white)
 title_text_rect = title_text.get_rect()
 title_text_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
-start_text = font.render("MONSTER WRANGLER", True, white)
+start_text = font.render("PRESS ENTER TO START", True, white)
 start_text_rect = start_text.get_rect()
-start_text_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 - 30)
+start_text_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 30)
 
 
 
@@ -37,32 +37,34 @@ start_text_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 - 30)
 
 
 
-
+pause = True
 game_running = True
 while game_running:
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             game_running = False
 
-            pause = True
-            while pause:
-                display_surface.blit(title_text, title_text_rect)
-                display_surface.blit(start_text, start_text_rect)
-                for ev in pygame.event.get():
-                    if ev.type == pygame.K_RETURN:
-                        game_running = False
+        elif ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                pause = False
+
+        if pause:
+            display_surface.blit(title_text, title_text_rect)
+            display_surface.blit(start_text, start_text_rect)
+
 
 
    
-
-    display_surface.blit(target_text, target_text_rect)
-
-    pygame.draw.line(display_surface, purple, (0, 90), (1200, 90), 5)
-    pygame.draw.line(display_surface, purple, (0, 610), (1200, 610), 5)
-    pygame.draw.line(display_surface, purple, (1197, 90), (1197, 610), 5)
-    pygame.draw.line(display_surface, purple, (2, 90), (2, 610), 5)
-
+        else:
+            display_surface.fill((0, 0, 0))
+            display_surface.blit(target_text, target_text_rect)
+            pygame.draw.line(display_surface, purple, (0, 90), (1200, 90), 5)
+            pygame.draw.line(display_surface, purple, (0, 610), (1200, 610), 5)
+            pygame.draw.line(display_surface, purple, (1197, 90), (1197, 610), 5)
+            pygame.draw.line(display_surface, purple, (2, 90), (2, 610), 5)
+            
+            clock.tick(FPS)
+            
     pygame.display.update()
-    clock.tick(FPS)
-
 pygame.quit
+               
